3964dda8dd62cd712e7f74831ac844547745dfb7
Vincent Bohlen <vincent.bohlen@fokus.fraunhofer.de>
2019-11-15 15:45:50 +0100

    Initial registration and config-manager added

--- src/main/java/de/fraunhofer/fokus/ids/services/InitService.java
+++ src/main/java/de/fraunhofer/fokus/ids/services/InitService.java
@@ -1,29 +1,40 @@
 package de.fraunhofer.fokus.ids.services;
 
 import de.fraunhofer.fokus.ids.services.database.DatabaseService;
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Future;
-import io.vertx.core.Handler;
-import io.vertx.core.Vertx;
+import io.vertx.config.ConfigRetriever;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
+import io.vertx.core.*;
 import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.web.client.WebClient;
 
+/**
+ * @author Vincent Bohlen, vincent.bohlen@fokus.fraunhofer.de
+ */
 public class InitService {
-    final io.vertx.core.logging.Logger LOGGER = LoggerFactory.getLogger(InitService.class.getName());
+    private final Logger LOGGER = LoggerFactory.getLogger(InitService.class.getName());
 
-    DatabaseService databaseService;
+    private DatabaseService databaseService;
 
     public InitService(Vertx vertx, Handler<AsyncResult<Void>> resultHandler){
         this.databaseService = DatabaseService.createProxy(vertx, Constants.DATABASE_SERVICE);
-        initDB(reply -> {
+
+        Future<Void> dbFuture = Future.future();
+        initDB(dbFuture.completer());
+        Future<Void> configFuture = Future.future();
+        register(vertx, configFuture.completer());
+
+        CompositeFuture.all(dbFuture, configFuture).setHandler( reply -> {
             if(reply.succeeded()){
                 resultHandler.handle(Future.succeededFuture());
             }
             else{
-                LOGGER.info("Table creation failed.", reply.cause());
+                LOGGER.error("Table creation failed.", reply.cause());
                 resultHandler.handle(Future.failedFuture(reply.cause()));
             }
-
         });
     }
 
@@ -33,8 +44,48 @@ public class InitService {
                 resultHandler.handle(Future.succeededFuture());
             }
             else{
-                LOGGER.info("Table creation failed.", reply.cause());
+                LOGGER.error("Table creation failed.", reply.cause());
                 resultHandler.handle(Future.failedFuture(reply.cause()));
+            }
+        });
+    }
+
+    /**
+     * This method is necessary if the adapter is to be started simultaneously with the management components to automatically perform the registration
+     * @param vertx  current vertx instance
+     * @param resultHandler Future.completer
+     */
+    private void register(Vertx vertx, Handler<AsyncResult<Void>> resultHandler){
+
+        ConfigStoreOptions confStore = new ConfigStoreOptions()
+                .setType("env");
+        ConfigRetrieverOptions options = new ConfigRetrieverOptions().addStore(confStore);
+
+        ConfigRetriever retriever = ConfigRetriever.create(vertx, options);
+
+        retriever.getConfig(ar -> {
+            if (ar.succeeded()) {
+                if (ar.result().containsKey("ROUTE_ALIAS")) {
+                    JsonObject registration = new JsonObject()
+                            .put("name", "CKAN")
+                            .put("address", new JsonObject()
+                                    .put("host", ar.result().getString("ROUTE_ALIAS"))
+                                    .put("port", 8080));
+                    WebClient webClient = WebClient.create(vertx);
+                    webClient
+                            .post(ar.result().getInteger("CONFIG_MANAGER_PORT"), ar.result().getString("CONFIG_MANAGER_HOST"), "/register")
+                            .sendJsonObject(registration, reg -> {
+                                if (reg.succeeded()) {
+                                    resultHandler.handle(Future.succeededFuture());
+                                } else {
+                                    LOGGER.error(reg.cause());
+                                    resultHandler.handle(Future.failedFuture(reg.cause()));
+                                }
+                            });
+                }
+            } else{
+                LOGGER.error(ar.cause());
+                resultHandler.handle(Future.failedFuture(ar.cause()));
             }
         });
     }
